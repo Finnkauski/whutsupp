ff2fe07c5788d596b522a936b12c994909100924
Finnkauski <iwiivi@gmail.com>
2018-12-16 15:05:03 +0000

    tweak: removed woffle, submodule

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -- imports
 # base
 import functools
-import regex as re
+import re
 
 # third party 
 import toml
@@ -10,6 +10,7 @@ from numpy import array
 
 # project
 from functions import compose, parse, clean
+from woffle.woffle.embed.numeric.spacy.embed import embed
 
 # -- parse
 with open('data/text.txt', 'r') as f:
@@ -19,4 +20,3 @@ config = toml.load('config.ini')
 
 pipe = compose(array, list, clean, parse)
 l = pipe(lines)
-
